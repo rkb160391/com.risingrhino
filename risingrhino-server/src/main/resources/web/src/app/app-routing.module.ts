29a192d094ad845298338e5ad92b1331bfefdc0f
rkb160391 <rkb160391@gmail.com>
2020-06-09 00:12:52 +0530

    ui added

---
+++ risingrhino-server/src/main/resources/web/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './core/components/home/home.component';
+import { AboutComponent } from './core/components/about/about.component';
+import { ContactComponent } from './core/components/contact/contact.component';
+import { FootwearComponent } from './core/components/footwear/footwear.component';
+import { BagsComponent } from './core/components/bags/bags.component';
+import { AccessoriesComponent } from './core/components/accessories/accessories.component';
+
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'footwear', component: FootwearComponent },
+  { path: 'bags', component: BagsComponent },
+  { path: 'accessories', component: AccessoriesComponent }];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
